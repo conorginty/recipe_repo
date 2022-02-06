bf903341f22002c1fb022bdf23ce88d465af259e
Conor Ginty <conorginty@gmail.com>
2022-02-06 23:04:39 +0000

    Lec 141: Spring Data JPA Query Methods

--- src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,13 +1,35 @@
 package guru.springframework.spring5recipeapp.controllers;
 
+import guru.springframework.spring5recipeapp.domain.Category;
+import guru.springframework.spring5recipeapp.domain.UnitOfMeasure;
+import guru.springframework.spring5recipeapp.repositories.CategoryRepository;
+import guru.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller // Remember, Due to the Default Spring Boot package scan by App file of any Bean underneath it, this will get picked up as a Bean
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "index"})
     public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Cup");
+
+        System.out.printf("Category ID: %d, Description: %s", categoryOptional.get().getId(), categoryOptional.get().getDescription());
+        System.out.println();
+        System.out.printf("UOM ID: %d, Description: %s", unitOfMeasure.get().getId(), unitOfMeasure.get().getDescription());
+
         return "index"; // Resolves to the Thymeleaf Template filename we created (index.html)
     }
 }
